2a099f3d8d56c46e4ced6744408e41363e502749
DamienCahill <53192581+DamienCahill@users.noreply.github.com>
2023-04-02 11:20:48 +0100

    added subtract function

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ import os
 def add(x, y):
     """Returns the sum of x and y."""
     if os.name == 'posix':
-    	print("Line wont be reached on windows")
-    	print("Another Line that wont be reached on windows")
+        print("Line wont be reached on windows")
+        print("Another Line that wont be reached on windows")
     return x + y
+
+def subtract(x, y):
+    """Returns the x minus y."""
+    return y - x
